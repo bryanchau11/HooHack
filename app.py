70640b1688324f7e0d9f45ce55ec9b09f28bbd63
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-03-28 02:57:26 -0500

    qrcode updated

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import pyqrcode
 import png
 from pyqrcode import QRCode
 
-QRstring = "https://codd.cs.gsu.edu/~nchau1/HooHack/"
+QRstring = "https://codd.cs.gsu.edu/~nchau1/HooHack/profile.html"
 url = pyqrcode.create(QRstring)
 url.png('qrcodeyoutube.png', scale = 8)
 
 
-# import cv2
-# d=cv2.QRCodeDetector()
-# val, points, straight_qrcode = d.detectAndDecode(cv2.imread("hello.svg"))
-# print(val)
